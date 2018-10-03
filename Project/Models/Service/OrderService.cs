ec8d3204af0484246ecf63f2b7303b13f5180c82
昇翰 吳 <1103137243@gm.kuas.edu.tw>
2018-10-03 15:10:28 +0800

    查詢OK(客戶名稱有問題)

--- Project/Models/Service/OrderService.cs
+++ Project/Models/Service/OrderService.cs
@@ -21,14 +21,68 @@ namespace Project.Models.Service
             DbService dbservice = new DbService();
             String connStr = dbservice.GetConnStr();                
             SqlConnection conn = new SqlConnection(connStr);
-            String sql = @"Select a.OrderID,b.CompanyName,a.OrderDate,a.ShippedDate from Sales.Orders a join Sales.Customers b on a.CustomerID = b.CustomerID";
+            String sql = @"Select a.OrderID,b.CompanyName,a.OrderDate,a.ShippedDate from Sales.Orders a join Sales.Customers b on a.CustomerID = b.CustomerID Where ";
+            if (arg.OrderID.HasValue)
+            {
+                sql += "OrderID = @OrderID";
+            }
+            else
+            {
+                sql += "OrderID like @OrderID";
+            }
+            if (!String.IsNullOrEmpty(arg.CompanyName))
+            {
+                sql += " and CompanyName = @CompanyName";
+            }
+            if (arg.EmployeeID.HasValue)
+            {
+                sql += " and EmployeeID = @EmployeeID";
+            }
+            if (arg.ShipperID.HasValue)
+            {
+                sql += " and ShipperID = @ShipperID";
+            }
+            if (arg.OrderDate.HasValue)
+            {
+                sql += " and OrderDate = @OrderDate";
+            }
+            if (arg.ShippedDate.HasValue)
+            {
+                sql += " and ShippedDate = @ShippedDate";
+            }
 
+            SqlCommand command = new SqlCommand(sql, conn);
 
+            if (arg.OrderID.HasValue)
+            {
+                command.Parameters.Add(new SqlParameter("@OrderID", arg.OrderID));
+            }
+            else
+            {
+                command.Parameters.Add(new SqlParameter("@OrderID","%"));
+            }
+            if (!String.IsNullOrEmpty(arg.CompanyName))
+            {
+                command.Parameters.Add(new SqlParameter("@CompanyName", arg.CompanyName));
+            }
+            if (arg.EmployeeID.HasValue)
+            {
+                command.Parameters.Add(new SqlParameter("@EmployeeID", arg.EmployeeID));
+            }
+            if (arg.ShipperID.HasValue)
+            {
+                command.Parameters.Add(new SqlParameter("@ShipperID", arg.ShipperID));
+            }
+            if (arg.OrderDate.HasValue)
+            {
+                command.Parameters.Add(new SqlParameter("@OrderDate", arg.OrderDate));
+            }
+            if (arg.ShippedDate.HasValue)
+            {
+                command.Parameters.Add(new SqlParameter("@ShippedDate", arg.ShippedDate));
+            }       
 
-
-
-
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, conn);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
             DataSet ds = new DataSet();
             dataAdapter.Fill(ds);
             DataTable dataTable = ds.Tables[0];
